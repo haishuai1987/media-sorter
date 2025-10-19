f9d66be079a1ca50a82d53602aa6b1e00ba178cf
haishuai1987 <2887256@163.com>
2025-10-20 02:55:47 +0800

    fix: 修复index.html路径，使用public/index.html

--- app.py
+++ app.py
@@ -2362,7 +2362,7 @@ class MediaHandler(SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html; charset=utf-8')
             self.end_headers()
-            with open('index.html', 'rb') as f:
+            with open('public/index.html', 'rb') as f:
                 self.wfile.write(f.read())
         elif self.path == '/logo.svg':
             try:
